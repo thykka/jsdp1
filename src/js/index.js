14d22e0f666582ad0fc073bc7287c51f943ed48c
Moses Holmström <thykka@gmail.com>
2019-09-17 22:49:20 +0300

    d3-helloworld

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,32 @@
+import * as d3 from 'd3';
 import log from './utils/log';
+
+const dataPoints = Array.from({length: 11}, () => 0)
+  .reduce((acc, _, i, a) => {
+    const v = i<2 ? 1 : acc[i-1] + acc[i-2];
+    return [...acc, v];
+  }, [])
+  .map((v, i, a) => [v, i / (a.length - 1)]);
+
+log(dataPoints);
 
 function app () {
   log(0, 'Starting app…');
-  log(0, 'App started!');
+
+  const svg = d3.select('svg#cloud');
+  svg.attr('width', innerWidth)
+    .attr('height', innerHeight);
+
+  const circle = svg.selectAll('circle')
+    .data(dataPoints);
+
+  circle.enter().append('circle')
+    .attr('r', (d) => `${d[0]}px`)
+    .attr('cx', (d) => d[1] * 100 + '%')
+    .attr('cy', () => 100)
+    .style('fill', '#fffc');
+
+  circle.exit().remove();
 }
 
 app();
