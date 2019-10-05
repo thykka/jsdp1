49443acea0a3aef409c8346e0e7572a9107c17fd
Moses Holmström <thykka@gmail.com>
2019-10-05 18:06:04 +0300

    line contours

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,48 @@
 import * as d3 from 'd3';
 import log from './utils/log';
 
-const dataPoints = Array.from({length: 11}, () => 0)
-  .reduce((acc, _, i, a) => {
-    const v = i<2 ? 1 : acc[i-1] + acc[i-2];
-    return [...acc, v];
-  }, [])
-  .map((v, i, a) => [v, i / (a.length - 1)]);
+const pointCount = 20;
+const dataPoints = Array.from({length: pointCount}, (_, i) => {
+  const v = i / (pointCount - 1);
+  return {
+    x: v,
+    y: i === 0 || i === pointCount - 1 ? 0 : Math.random()
+  };
+});
 
-log(dataPoints);
+const scaleX = d3.scaleLinear()
+  .domain([0, 1])
+  .range([0, innerWidth]);
+
+const scaleY = d3.scaleLinear()
+  .domain([0, 1])
+  .range([0, innerHeight]);
+
+const slow = d3.transition()
+  .duration(5000)
+  .ease(d3.easeSin);
+
+const line = d3.line()
+  .x(d => scaleX(d.x))
+  .y(d => scaleY(d.y));
+
 
 function app () {
   log(0, 'Starting app…');
 
   const svg = d3.select('svg#cloud');
   svg.attr('width', innerWidth)
-    .attr('height', innerHeight);
+    .attr('height', innerHeight)
+    .style('stroke-width', 0.75);
 
-  const circle = svg.selectAll('circle')
-    .data(dataPoints);
-
-  circle.enter().append('circle')
-    .attr('r', (d) => `${d[0]}px`)
-    .attr('cx', (d) => d[1] * 100 + '%')
-    .attr('cy', () => 100)
-    .style('fill', '#fffc');
-
-  circle.exit().remove();
+  const path = svg.select('path').datum(dataPoints);
+  [...Array(innerHeight / 8 | 0)].map((_, i, a) => {
+    const beta = (i / (a.length - 1));
+    return path.clone().attr(
+      'd',
+      line.curve(d3.curveBundle.beta(beta))
+    ).style('opacity', beta);
+  });
 }
 
 app();
